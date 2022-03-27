c014492affd63251540d357aff7e25ae4816c425
Ondrej Svoboda <admin@svoby.eu>
2022-03-27 12:47:54 +0200

    static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
 
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  get '/logout', to: 'sessions#destroy'
+  get   '/login',   to: 'sessions#new'
+  post  '/login',   to: 'sessions#create'
+  get   '/logout',  to: 'sessions#destroy'
+
+  get   '/signup',  to: 'users#new'
 
   resources :users
+
+  root 'static_pages#home'
 
 end
